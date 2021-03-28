9088668af96b00664b66b8a1e7ef26265c77d5cf
Paul Murphy <murphypaul1985@gmail.com>
2021-03-28 19:04:20 +0100

    Bug fixes.

--- BlogLab.Repository/BlogCommentRepository.cs
+++ BlogLab.Repository/BlogCommentRepository.cs
@@ -93,7 +93,10 @@ namespace BlogLab.Repository
 
                 newBlogCommentId = await connection.ExecuteScalarAsync<int?>(
                     "BlogComment_Upsert",
-                    new { BlogComment = dataTable.AsTableValuedParameter("dbo.BlogCommentType") },
+                    new { 
+                        BlogComment = dataTable.AsTableValuedParameter("dbo.BlogCommentType"),
+                        ApplicationUserId = applicationUserId
+                    },
                     commandType: CommandType.StoredProcedure);
             }
 
